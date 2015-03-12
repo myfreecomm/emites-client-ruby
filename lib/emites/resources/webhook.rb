649268de21ea6b579de5bad99e6313cbe14a71b0
Wanderson Policarpo <wanderson.policarpo@myfreecomm.com.br>
2015-03-12 14:27:33 -0300

    Added webhook/list service

--- lib/emites/resources/webhook.rb
+++ lib/emites/resources/webhook.rb
@@ -2,6 +2,12 @@ module Emites
   module Resources
     class Webhook < Base
 
+      def list
+        http.get("/webhooks") do |response|
+          respond_with_collection(response)
+        end
+      end
+
       def update(id, params)
         http.put("/webhooks/#{id}", {body: params}) do |response|
           respond_with_entity(response)
