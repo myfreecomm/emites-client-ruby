8f21e0b87dab98d0c33c26fedc05bb2af114cc55
leandrost <leandro.s.thimoteo@gmail.com>
2015-03-13 15:05:46 -0300

    Requires thyphoeus

--- lib/emites.rb
+++ lib/emites.rb
@@ -1,18 +1,20 @@
+require "typhoeus"
 require "multi_json"
 
 require "emites/version"
 require "emites/configuration"
-require "emites/client"
 require "emites/http"
-
-require "emites/resources/base"
-require "emites/resources/emitter"
-require "emites/resources/webhook"
+require "emites/client"
 
 require "emites/entities/base"
 require "emites/entities/emitter"
 require "emites/entities/webhook"
 require "emites/entities/taker"
+
+require "emites/resources/base"
+require "emites/resources/emitter"
+require "emites/resources/webhook"
+
 
 module Emites
   def self.configuration
